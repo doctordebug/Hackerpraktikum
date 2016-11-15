48a2242baa5fadf7589f78f56a23981f030948b4
Stuhlmaler <oli.saenger@googlemail.com>
2016-11-15 15:16:39 +0100

    Started with 2.4

--- Aufgabe_2/Aufgabe_2.4/wep/klein_attack.py
+++ Aufgabe_2/Aufgabe_2.4/wep/klein_attack.py
@@ -1,4 +1,6 @@
+import base64
 import datetime
+import sys
 from collections import Counter
 
 from utils import log
@@ -116,12 +118,61 @@ def crack_simulation(n=256, tuple_amount=90000, rounds=1, key_length=40):
     return possible_key, main_key
 
 
-if __name__ == '__main__':
-    keys = []
+def generate_arp_request_package_plaintext():
+    llc_header = base64.b16decode(b'AAAA030000000806')
+    arp_header = base64.b16decode(b'0001080006040001')
+    source_mac = base64.b16decode(b'AAAAAAAAAAAA')
+    source_ip = base64.b16decode(b'BBBBBBBB')
+    target_mac = base64.b16decode(b'FFFFFFFFFFFF')
+    target_ip = base64.b16decode(b'EEEEEEEE')
+
+    return llc_header + arp_header + source_mac + source_ip + target_mac + target_ip
+
+
+def load_requests(filename='../output-03.cap', offset=64):
+    # 77:65:70:31:36 [HEX] wep 16
+    fh = open(filename, 'rb')
+    import mmap
+    m = mmap.mmap(fh.fileno(), 0, access=mmap.ACCESS_READ)
+    return m[:offset]
+
+
+def generate_arp_response_package_plaintext():
     """
+    LLC-header: AA AA 03 00 00 00 08 06
+    ARP-HEADER: 00 01 08 00 06 04 00 02
+    Quell-MAC-Adresse (6 Byte) enthält in einer ARP-Anforderung die MAC-Adresse des Senders. In einer ARP-Antwort enthält es die MAC-Adresse des antwortenden Hosts oder Next-Hop-Routers.
+    Quell-IP-Adresse (4 Bytes bei IPv4) enthält bei einer ARP-Anforderung die IP-Adresse des anfragenden Hosts. In einer ARP-Antwort enthält es die IP-Adresse des antwortenden Hosts oder Next-Hop-Routers.
+    Ziel-MAC-Adresse (6 Byte) ist in einer ARP-Anforderung ein Broadcast (FF:FF:FF:FF:FF:FF). In einer ARP-Antwort enthält es die MAC-Adresse des anfragenden Hosts.
+    Ziel-IP-Adresse (4 Bytes bei IPv4) ist bei einer ARP-Anforderung die IP-Adresse des gesuchten Hosts. In einer ARP-Antwort enthält es die IP-Adresse des anfragenden Hosts.
+    """
+    llc_header = base64.b16decode(b'AAAA030000000806')
+    arp_header = base64.b16decode(b'0001080006040002')
+    source_mac = base64.b16decode(b'AAAAAAAAAAAA')
+    source_ip = base64.b16decode(b'BBBBBBBB')
+    target_mac = base64.b16decode(b'FFFFFFFFFFFF')
+    target_ip = base64.b16decode(b'EEEEEEEE')
+
+    return llc_header + arp_header + source_mac + source_ip + target_mac + target_ip
+
+def bytes_xor(a, b) :
+    return bytes(x ^ y for x, y in zip(a, b))
+
+if __name__ == '__main__':
+    """
+    keys = []
+    chiffre = load_requests(offset=40)
+    arp_plain = generate_arp_request_package_plaintext()
+
+    possible = bytes_xor(chiffre, arp_plain)
+    print(possible)
+
+    a = [77,65,70,31,36]
+    """
+    keys = []
     for i in reversed(range(0, 90000, 10000)):
         keys.append(dict(tuple_amount=i, key=crack_simulation(tuple_amount=i)))
     print(keys)
 
     print(crack_simulation(tuple_amount=90000, key_length=104))
-    """
+
