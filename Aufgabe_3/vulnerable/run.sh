14b6f3de2476f54fad45fbd5ef1cd1029e70d7c2
Lukas Jung <s4lujung@uni-trier.de>
2017-01-23 12:47:17 +0100

    removed some comments

--- Aufgabe_3/vulnerable/run.sh
+++ Aufgabe_3/vulnerable/run.sh
@@ -10,8 +10,3 @@ echo "nameserver 127.0.0.1" > /etc/resolv.conf
 # Change routing of traffic to the bridged interface (eth1)
 # If this is not done DNS responses will arrive on interface eth0
 ip route change default dev eth1 via $1
-# ip route change to default dev eth1 via $1
-# route add default gw 192.168.0.1
-
-
-# default via 10.0.2.2 dev eth0
